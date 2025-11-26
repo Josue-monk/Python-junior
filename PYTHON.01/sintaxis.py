771a9d99adc9ba0ad1046dbd6cc58c5623018ce8
JosueAG <monky692005@gmail.com>
2025-11-25 23:48:46 -0500

    Add files via upload

---
+++ PYTHON.01/sintaxis.py
@@ -0,0 +1,10 @@
+print("hello wordl")
+print(34+5)
+print(20+5)
+print("hola",35,"adios")#este es un comentario
+#print("esto tambien es otro comentario")
+x=25
+Y=2
+print("la suma del valor de x  e y es :",x+Y)
+print("la resta del valor de x  e y es :",x-Y)
+print("la multiplicacion es ",x/Y)
---
+++ PYTHON.01/tipo_datos.py
@@ -0,0 +1,13 @@
+# tipos de datos en python 
+x=5# entereo
+y=3.5# float
+nombre="juan"# string
+num1=int (3)
+num2=float(4.5)
+num3=str("hola")
+print("el valor es de x es:",x)
+print("el valor es de y es:",y)
+print("el nombre es :",nombre)  
+print("num1 es el valor de",num1)
+print(num2)
+print("*******************")
