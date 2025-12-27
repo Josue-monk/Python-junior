1ab7dcdbd4a15e26a3a1b809efafb010e9352e45
JosueAG <monky692005@gmail.com>
2025-12-26 22:18:10 -0500

    Add files via upload

---
+++ practico.py
@@ -0,0 +1,23 @@
+
+nombre= "josue"
+apellido ="acuña"
+edad= 20
+altura= float (1.25)
+print(f"soy {nombre}{apellido}{edad}{altura}")
+base=float(input("ingrese la base requerida"))
+altura=float(input("ingrese la altura  requerida"))
+print(f"el area del rectangulo es {base*altura}")
+#- Calcula el área de un rectángulo con base y altura dadas.
+#- Verifica si un número es par usando %.
+# Comprueba si edad > 18 and edad < 30.
+num= float(input("ingrese el numero "))
+if num % 2 == 0:
+    print(" numero es par ")
+else:
+    print("numero impar ")
+edades = int(input("ingresar la edad"))
+if edades > 18 and edades < 30:
+    print("la edad esta en el rango requerido ")
+else:
+    print("la edad esta fuera del rango ")
+
